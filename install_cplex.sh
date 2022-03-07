c157410741a21ff07a1a76d3fa39a0e384b0a42e
Lucas Ford <lcford2@ncsu.edu>
2022-03-07 13:07:45 -0500

    logic to find best version of cplex to install

--- install_cplex.sh
+++ install_cplex.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-VER=128
+find_cplex_file () {
+    CPLEX_FILES="$(ls cplex_studio*.linux-x86-64.bin)"
 
-if [ -x "./cplex_studio${VER}.linux-x86-64.bin" ]; then
-    sed -i "s|USER_INSTALL_DIR=/opt/cplex.*|USER_INSTALL_DIR=/opt/cplex${VER}|" cplex.properties
-    ./cplex_studio${VER}.linux-x86-64.bin -f cplex.properties
-    ln -s /opt/cplex${VER} /opt/cplex
+    max_ver=0
+    for file in ${CPLEX_FILES[@]};
+    do
+        version="$(echo $file | grep -E -oh '[0-9]{3}')"
+        if [ "$version" -lt 129 ] && [ "$version" -gt "$max_ver" ]; then
+            max_ver=$version
+        fi
+    done
+    best_cplex="cplex_studio${max_ver}.linux-x86-64.bin"
+}
+
+# this script is only executed after it is confirmed
+# that a cplex_studio file exists in this directory
+# so after calling find_cplex_file, best_cplex and max_ver
+# are guaranteed to exist. 
+find_cplex_file
+
+if [ -x "$best_cplex" ]; then
+    sed -i "s|USER_INSTALL_DIR=/opt/cplex.*|USER_INSTALL_DIR=/opt/cplex${max_ver}|" cplex.properties
+    # install cplex and setup coregs to find it
+    # if install fails, do not make a link and do not replace glpk
+    ./$best_cplex -f cplex.properties && \
+        ln -s /opt/cplex${VER} /opt/cplex && \
+        sed -i 's/glpk/cplex/g' coregs.py
 else
     echo "Cannot install cplex because cplex_studio${VER}.linux-x86-64.bin is not executable"
    exit 1
